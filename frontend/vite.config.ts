a965bfcf29089abcb8f1c48f2a206a1c334e4e95
Sebas <diego.cruz.cervantes@gmail.com>
2025-11-18 11:11:04 -0600

    Update Vite configuration to set allowedHosts to true for both server and preview settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ["*"] 
+    allowedHosts: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: true,
   },
 });
 
